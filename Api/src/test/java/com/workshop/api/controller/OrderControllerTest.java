4ad069eea53934096ba7d353bc400bcf4b581c35
alexnascimento2727 <alex.nascimento@ciadetalentos.com>
2023-11-15 19:05:02 -0300

    dockerfile comentado

--- Api/src/test/java/com/workshop/api/controller/OrderControllerTest.java
+++ Api/src/test/java/com/workshop/api/controller/OrderControllerTest.java
@@ -67,7 +67,6 @@ public class OrderControllerTest {
                 .andExpect(status().isOk());
         
         Order orderSalvo = orderService.searchOrFailById(id);
-
         assertEquals(orderSalvo.getId(), id);
         assertNotNull(orderSalvo);
 
