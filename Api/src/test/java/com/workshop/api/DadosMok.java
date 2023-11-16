7879801a73b6ed2aaeb672af0717cafe8d9a2f63
alexnascimento2727 <alex.nascimento@ciadetalentos.com>
2023-11-15 23:00:47 -0300

    teste 07

--- Api/src/test/java/com/workshop/api/DadosMok.java
+++ Api/src/test/java/com/workshop/api/DadosMok.java
@@ -8,7 +8,7 @@ import com.workshop.api.model.Order;
 public class DadosMok {
 	public Order getOrder() {
         return Order.builder() 
-        		.id(3L)
+        		.id(1L)
                 .name("alex nascimento")
                 .product(1L)
                 .price(BigDecimal.TEN)
